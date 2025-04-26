55442beb130d94b04dce864dc36831e49f3f03ca
Michaela Hošková <66723714+misahoskova@users.noreply.github.com>
2025-04-26 22:24:38 +0200

    testy pro nove funkce

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,18 +1,89 @@
 import test from "ava"
 import { migrate } from "drizzle-orm/libsql/migrator"
-import { db, getTodoById } from "../src/app.js"
+import {
+  db,
+  getTodoById,
+  getAllTodos,
+  updateTodo,
+  deleteTodo,
+} from "../src/app.js"
 import { todosTable } from "../src/schema.js"
 
 test.before("run migrations", async () => {
   await migrate(db, { migrationsFolder: "drizzle" })
+  await db.delete(todosTable)
 })
 
-test("getTodoById returns id", async (t) => {
-  await db
+test.beforeEach("delete table values", async () => {
+  await db.delete(todosTable)
+})
+
+test("getTodoById returns correct todo", async (t) => {
+  const inserted = await db
     .insert(todosTable)
-    .values({ id: 1, title: "testovaci todo", done: false })
+    .values({
+      title: "testovaci todo pro test 1",
+      done: false,
+    })
+    .returning()
 
-  const todo = await getTodoById(1)
+  const id = inserted[0].id
 
-  t.is(todo.title, "testovaci todo")
+  const todo = await getTodoById(id)
+
+  t.is(todo.title, "testovaci todo pro test 1")
+})
+
+test("getAllTodos returns all todos", async (t) => {
+  await db.insert(todosTable).values([
+    { title: "todo 1", done: false },
+    { title: "todo 2", done: true },
+  ])
+
+  const todos = await getAllTodos()
+  console.log("Todos in table:", todos)
+
+  t.is(todos.length, 2)
+  t.is(todos[0].title, "todo 1")
+  t.is(todos[1].title, "todo 2")
+})
+
+test("updateTodo updates todo correctly", async (t) => {
+  const inserted = await db
+    .insert(todosTable)
+    .values({
+      title: "testovaci todo pro update",
+      done: false,
+    })
+    .returning()
+
+  const id = inserted[0].id
+
+  await updateTodo(id, {
+    title: "updatované todo",
+    done: true,
+  })
+
+  const updatedTodo = await getTodoById(id)
+
+  t.is(updatedTodo.title, "updatované todo")
+  t.true(updatedTodo.done)
+})
+
+test("deleteTodo deletes todo correctly", async (t) => {
+  const inserted = await db
+    .insert(todosTable)
+    .values({
+      title: "testovaci todo pro delete",
+      done: false,
+    })
+    .returning()
+
+  const id = inserted[0].id
+
+  await deleteTodo(id)
+
+  const deletedTodo = await getTodoById(id)
+
+  t.is(deletedTodo, undefined)
 })
